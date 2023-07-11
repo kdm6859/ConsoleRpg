584b2c9b1c1b7744840a991d727d5e3b071516a7
kdm6859 <64892955+kdm6859@users.noreply.github.com>
2023-07-11 13:17:50 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpg
+{
+    public class Program
+    {
+        [DllImport("msvcrt.dll")]
+        public static extern int _getch(); //C언어 함수 가져옴
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
