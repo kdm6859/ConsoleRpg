a984439dde1fd09f815ca84ee38472d6e7b2aafb
wooriv88 <gureuah@naver.com>
2023-07-14 15:11:17 +0900

    새로운거 넣기

---
+++ JWR/GameManager.cs
@@ -0,0 +1,187 @@
+﻿
+using ConsoleRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleRPG
+{
+
+    internal class GameManager
+    {
+        public static GameManager instance;
+        public static GameManager Instance()
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+
+        Map map = null;
+        Player player = null;
+        Monster mon = null;
+        Monster mon2 = null;
+        Monster mon3 = null;
+        Monster mon4 = null;
+        Monster mon5 = null;
+        Monster mon6 = null;
+        Skill skill = null;
+        ShortSkill shortskill = null;
+
+        public Player GetPlayer() { return player; }
+        public Monster GetMonster() { return mon; }
+        public void Initialize()
+        {
+            map = new Map();
+            skill = new Skill();
+            player = new Player();
+            shortskill = new ShortSkill(player, skill);
+
+            map.Initialize();
+            player.Initailize();
+
+            if (map.currentStageNum == Map.StageNum.stage1)
+            {
+                mon = new Monster("Monster", 40, 28, 1, 500); //string[] symbol
+                mon2 = new Monster("Monster", 80, 28, 1, 500); //string[] symbol
+                mon3 = new Monster("Monster", 70, 18, 1, 500); //string[] symbol
+                mon4 = new Monster("Monster", 15, 8, 1, 500); //string[] symbol
+                mon5 = new Monster("Monster", 80, 10, 1, 500); //string[] symbol
+                mon6 = new Monster("Monster", 110, 15, 1, 500); //string[] symbol
+
+
+            }
+
+
+
+            ObjectManager.Instance().Initialize(ref player, ref mon, ref map);
+
+
+        }
+        public void Progress()
+        {
+            INVENPANEL.Instance().KeySensing();
+            KeyControlManager.Instance().KeyControl();
+
+
+
+            player.Progress(mon, mon2, mon3, mon4, mon5, mon6);
+
+            mon.Progress(player);
+            mon2.Progress(player);
+            mon3.Progress(player);
+            mon4.Progress(player);
+            mon5.Progress(player);
+            mon6.Progress(player);
+
+
+            ObjectManager.Instance().Progress();
+
+
+        }
+        public void Render()
+        {
+            Console.SetCursorPosition(3, 3);
+            Console.Write("isLanding = " + ObjectManager.Instance().isLanding);
+
+            map.Render();
+
+            INVENPANEL.Instance().OpenInventory();
+            INVENPANEL.Instance().DisplayInfoPanel();
+            INVENPANEL.Instance().UseHPotion();
+            INVENPANEL.Instance().UseMPotion();
+            mon.Render();
+            mon2.Render();
+            mon3.Render();
+            mon4.Render();
+            mon5.Render();
+            mon6.Render();
+            player.Render();
+
+
+
+            //mon.PrintLocation();
+        }
+
+    }
+    /* internal class GameManager
+     {
+         public static GameManager instance;
+         public static GameManager Instance()
+         {
+             if (instance == null)
+             {
+                 instance = new GameManager();
+             }
+             return instance;
+         }
+
+         Map map = null;
+         Player player = null;
+         Monster mon = null;
+         Skill skill = null;
+         ShortSkill shortskill = null;
+
+         public Player GetPlayer() { return player; }
+         public Monster GetMonster() { return mon; }
+         public void Initialize()
+         {
+             map = new Map();
+             skill = new Skill();
+             player = new Player();
+             shortskill = new ShortSkill(player, skill);
+
+             map.Initialize();
+             player.Initailize();
+
+
+             mon = new Monster("Monster", 40, 28, Monster.monsterStr, 1, 500);
+
+         }
+         public void Progress()
+         {
+             INVENPANEL.Instance().KeySensing();
+             KeyControlManager.Instance().KeyControl();
+             player.Progress(mon);
+             mon.Progress(player);
+
+
+
+         }
+         public void Render()
+         {
+             //Console.Clear();
+
+             //Console.WriteLine("플레이어 dir :" + player.getSkill().dir);
+             //Console.WriteLine("이즈엑티브 :" + player.getSkill().isActive);
+             //Console.WriteLine("스킬 x좌표 : " + player.getSkill().SkillX);
+
+
+             map.Render();
+
+             INVENPANEL.Instance().OpenInventory();
+             INVENPANEL.Instance().DisplayInfoPanel();
+             INVENPANEL.Instance().UseHPotion();
+             INVENPANEL.Instance().UseMPotion();
+
+             player.Render();
+             mon.Render();
+             Console.WriteLine("캐릭터 공격력 :" + player.GetINFO().pAttack);
+             Console.WriteLine("몬스터 체력 :" + mon.Hp);
+
+             //mon.PrintLocation();
+
+
+         }
+
+     }*/
+
+}
+
+
---
+++ JWR/Monster.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ConsoleRPG;
+
+namespace ConsoleRPG
+{
+    public class Monster
+    {
+        public int Hp { get; private set; }
+        public int monsterdamage = 20;
+        public string Name { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string[] Symbol { get; private set; }
+        //public int Speed { get; private set; }
+
+
+        public static string[] monsterStr =
+        {
+           "o  o /￣＼",
+            " )( ｜＠ ｜",
+           " 〓〓〓〓",
+        };
+
+        public static string[] monsterStr2 =
+        {
+           " /￣＼  o  o",
+           "｜＠ ｜ )( ",
+           "  〓〓〓〓",
+        };
+
+        private int prevX;
+        private int prevY;
+        bool dir = true;
+        private bool isJumping = false;
+        //private int jumpDistance = 0;
+        private int jumpHeight = 3; // 몬스터의 점프 높이 설정
+        private int initialY; // 몬스터의 초기 Y 좌표
+        public Monster(string name, int x, int y, int speed, int Hp)  //string[] symbol
+        {
+            Name = name;
+            X = x;
+            Y = y;
+            //Symbol = symbol;
+            //Speed = speed;
+            initialY = y;
+            this.Hp = Hp;
+        }
+
+        public Monster getinfo()
+        {
+            return this;
+        }
+
+        public void SetDamage(int iAttack) { Hp -= iAttack; }
+
+        public void DirectionSet(Player player)
+        {
+            if (player.playerX > X)
+            {
+                dir = false;
+            }
+            else
+            {
+                dir = true;
+            }
+        }
+
+        public void Render()
+        {
+            if (Hp > 0 && dir)
+            {
+
+                Console.SetCursorPosition(X, Y);
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(monsterStr[j]);
+                    Console.SetCursorPosition(X, Y + j + 1);
+                }
+                Console.SetCursorPosition(X - 2, Y + 3);
+                Console.WriteLine("몬스터 체력 : " + Hp);
+            }
+            else if (Hp > 0 && dir == false)
+            {
+                Console.SetCursorPosition(X, Y);
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(monsterStr2[j]);  //Symbol[j]
+                    Console.SetCursorPosition(X, Y + j + 1);
+                }
+                Console.SetCursorPosition(X - 2, Y + 3);
+                Console.WriteLine("몬스터 체력 : " + Hp);
+            }
+        }
+
+        public void JumpToPlayer(Player player)
+        {
+            int playerX = player.playerX;
+            int playerFootPosition = player.GetPlayerFootPosition();
+
+            if (playerX == X && Hp > 0 && player.GetINFO().pHp > 0) //플레이어나 몬스터가 죽으면 공격 멈춤
+            {
+                Jump();
+                if (player.playerY == Y)
+                {
+                    player.SetDamage(this.monsterdamage); //몬스터랑 플레이어랑 좌표 같으면 플레이어 체력-20
+                }
+            }
+            else
+            {
+                Y = initialY;
+                JumpOX = true;
+
+                prevX = X;
+                prevY = Y;
+
+                int DesX = Math.Abs(X - playerX);
+
+                if (DesX < 15)
+                {
+
+                    if (playerX < X)
+                    {
+                        X--;
+                        if (X < 3)
+                        {
+                            X = 3;
+                        }
+                        else if (X > 145)
+                        {
+                            X = 145;
+                        }
+                        // Thread.Sleep(100);
+                    }
+                    else if (playerX > X)
+                    {
+                        X++;
+                        //Thread.Sleep(100);
+                    }
+
+                }
+                /*if (playerFootPosition < Y)
+                    Y--;
+                else if (playerFootPosition > Y)
+                    Y++;*/
+            }
+        }
+
+        public bool JumpOX = true;
+        public void Jump()
+        {
+
+            if (JumpOX)
+            {
+                Y--;
+                JumpOX = false;
+            }
+            else
+            {
+                Y++;
+                JumpOX = true;
+            }
+            //if (isJumping)
+            //{
+
+            //}
+        }
+
+        public void Progress(Player player)
+        {
+            DirectionSet(player);
+            JumpToPlayer(player);
+
+            // 나머지 로직을 추가로 처리
+            // ...
+        }
+
+
+        public void PrintLocation()
+        {
+            Console.WriteLine($"Monster X: {X}, Y: {Y}");
+        }
+    }
+
+}
